47dc64358cb90d145a2299338e21e7ea1d6492e9
anthonyKildery <121516936+anthonyKildery@users.noreply.github.com>
2023-03-07 16:46:17 -0400

    07-03-2023

--- script.js
+++ script.js
@@ -1,40 +1,87 @@
+let card = []
+let modalKey = 1
+let modelQtd
 const sel = (element) => document.querySelector(element)
 const selAll = (element) => document.querySelectorAll(element)
-const real = (valor) => valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+const real = (value) => value.toLocaleString( 'pt-BR', {style: 'currency', currency: 'BRL'} )
 
 pizzaJson.map( (item, index) => {
     const pizzaItem = sel('.models .pizza-item').cloneNode(true)
-
     pizzaItem.setAttribute('data-key', index)
+
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = real(item.price)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
 
-    pizzaItem.querySelector('a').addEventListener('click', (event) => {
+    pizzaItem.querySelector('a').addEventListener( 'click', (event) => {
+        modelQtd = 1
         event.preventDefault()
-        let key = event.target.closest('.pizza-item').getAttribute('data-key')
-        const modalQts = 1
+        const key = event.target.closest('.pizza-item').getAttribute('data-key')
+        modalKey = key
+
+        sel('.pizzaWindowArea').style.opacity = 0
+        setTimeout( () => {
+            sel('.pizzaWindowArea').style.opacity = 1
+        }, 200 )
+        sel('.pizzaWindowArea').style.display = 'flex'
 
         sel('.pizzaBig img').src = pizzaJson[key].img
         sel('.pizzaInfo h1').innerHTML = pizzaJson[key].name
         sel('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
         sel('.pizzaInfo--actualPrice').innerHTML = real(pizzaJson[key].price)
+        sel('.pizzaInfo--qt').innerHTML = modelQtd
         sel('.pizzaInfo--size.selected').classList.remove('selected')
-        selAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+        selAll('.pizzaInfo--size').forEach( (size, sizeIndex) => {
             if(sizeIndex == 2) {
                 size.classList.add('selected')
             }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
-        })
-        sel('.pizzaInfo--qt').innerHTML = modalQts
-
-        sel('.pizzaWindowArea').style.opacity = 0
-        sel('.pizzaWindowArea').style.display = 'flex'
-        setTimeout( () => {
-            sel('.pizzaWindowArea').style.opacity = 1
-        }, 200 )
-    })
-
+        } )
+    } )
+    
     sel('.pizza-area').append(pizzaItem)
+} )
+
+function closseModal() {
+    sel('.pizzaWindowArea').style.opacity = 0
+    setTimeout( () => {
+        sel('.pizzaWindowArea').style.display = 'none'
+    }, 1000 )
+}
+
+selAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach( (item) => {
+    item.addEventListener('click', closseModal)
+} )
+
+sel('.pizzaInfo--qtmais').addEventListener( 'click', () => {
+    modelQtd++
+    sel('.pizzaInfo--qt').innerHTML = modelQtd
+} )
+sel('.pizzaInfo--qtmenos').addEventListener( 'click', () => {
+    if(modelQtd > 1) {
+        modelQtd--
+        sel('.pizzaInfo--qt').innerHTML = modelQtd
+    }
+} )
+
+selAll('.pizzaInfo--size').forEach( (size, sizeIndex) => {
+    size.addEventListener( 'click', (event) => {
+        sel('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    } )
+} )
+
+sel('.pizzaInfo--addButton').addEventListener( 'click', () => {
+    const size = parseInt(sel('.pizzaInfo--size.selected').getAttribute('data-key'))
+
+    card.push( {
+        id: pizzaJson[modalKey].id,
+        size: size,
+        quantidade: modelQtd
+    } )
+
+    console.log(card)
+    closseModal();
 } )
