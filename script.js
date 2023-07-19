f8bce94db061a472eceea863bfc9bcbd72b1d917
Anthony Kildery <90473131+anthonyKld@users.noreply.github.com>
2023-07-19 13:34:38 -0300

    .
    .

--- script.js
+++ script.js
@@ -1,86 +1,39 @@
-let card = []
-let modalKey = 1
-let modelQtd
+const teste = console.log('Funcionou!')
 const sel = (element) => document.querySelector(element)
 const selAll = (element) => document.querySelectorAll(element)
 const real = (value) => value.toLocaleString( 'pt-BR', {style: 'currency', currency: 'BRL'} )
 
 pizzaJson.map( (item, index) => {
     const pizzaItem = sel('.models .pizza-item').cloneNode(true)
-    pizzaItem.setAttribute('data-key', index)
-
+    pizzaItem.querySelector('a').setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = real(item.price)
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
-
+    pizzaItem.querySelector('.pizza-item--price').append(real(item.price))
+    pizzaItem.querySelector('.pizza-item--name').append(item.name)
+    pizzaItem.querySelector('.pizza-item--desc').append(item.description)
     pizzaItem.querySelector('a').addEventListener( 'click', (event) => {
-        modelQtd = 1
+        let key = event.currentTarget.getAttribute('data-key')
         event.preventDefault()
-        const key = event.target.closest('.pizza-item').getAttribute('data-key')
-        modalKey = key
-
-        sel('.pizzaWindowArea').style.opacity = 0
-        setTimeout( () => {
-            sel('.pizzaWindowArea').style.opacity = 1
-        }, 200 )
         sel('.pizzaWindowArea').style.display = 'flex'
+        sel('.pizzaWindowArea').style.opacity = 0
+        setTimeout( () => sel('.pizzaWindowArea').style.opacity = 1, 200 )
 
-        sel('.pizzaBig img').src = pizzaJson[key].img
-        sel('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        sel('.pizzaWindowArea h1').innerHTML = pizzaJson[key].name
         sel('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
-        sel('.pizzaInfo--actualPrice').innerHTML = real(pizzaJson[key].price)
-        sel('.pizzaInfo--qt').innerHTML = modelQtd
-        sel('.pizzaInfo--size.selected').classList.remove('selected')
-
-        selAll('.pizzaInfo--size').forEach( (size, sizeIndex) => {
-            if(sizeIndex == 2) {
-                size.classList.add('selected')
-            }
-            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
-        } )
+        sel('.pizzaInfo--actualPrice').innerHTML =  real(pizzaJson[key].price)
+        sel('.pizzaBig img').src = pizzaJson[key].img
     } )
-    
+
     sel('.pizza-area').append(pizzaItem)
 } )
 
-function closseModal() {
+//Eventos do modal
+function botaoCancelar() {
     sel('.pizzaWindowArea').style.opacity = 0
     setTimeout( () => {
         sel('.pizzaWindowArea').style.display = 'none'
-    }, 1000 )
+    }, 500 )
 }
 
-selAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach( (item) => {
-    item.addEventListener('click', closseModal)
-} )
-
-sel('.pizzaInfo--qtmais').addEventListener( 'click', () => {
-    modelQtd++
-    sel('.pizzaInfo--qt').innerHTML = modelQtd
-} )
-sel('.pizzaInfo--qtmenos').addEventListener( 'click', () => {
-    if(modelQtd > 1) {
-        modelQtd--
-        sel('.pizzaInfo--qt').innerHTML = modelQtd
-    }
-} )
-
-selAll('.pizzaInfo--size').forEach( (size) => {
-    size.addEventListener( 'click', () => {
-        sel('.pizzaInfo--size.selected').classList.remove('selected')
-        size.classList.add('selected')
-    } )
-} )
-
-sel('.pizzaInfo--addButton').addEventListener( 'click', () => {
-    const size = parseInt(sel('.pizzaInfo--size.selected').getAttribute('data-key'))
-
-    card.push( {
-        id: pizzaJson[modalKey].id,
-        size: size,
-        quantidade: modelQtd
-    } )
-
-    closseModal();
-} )
+selAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach( (element) => {
+    element.addEventListener('click', botaoCancelar)
+})
